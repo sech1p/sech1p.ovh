46e4a652f542962d46304809b57ace77e248f2c3
sech1p <sech1p@disroot.org>
2024-09-27 19:37:01 +0200

    🔃 Update document title in head template because I forgot

--- templates/head.php
+++ templates/head.php
@@ -1,4 +1,4 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" type="text/css" href="style.css">
-<title>Document</title>
+<title>sech1p's corner</title>
