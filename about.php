8bb3ce08e68b898636b4b51296769f209cc011f6
sech1p <sech1p@disroot.org>
2024-09-27 18:56:59 +0200

    🔃 Rewrite About page to php

---
+++ about.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php include "templates/head.php"; ?>
+</head>
+<body>
+    <div id="top">
+	    <div id="main">
+		    <?php include "templates/header.php"; ?>
+		    <div id="subject">
+		        <article class="content">
+                    <h1>Hello, world!</h1>
+					<p>Eliza, polyamorous trans woman born in 2005 in East Poland. I am a lesbian, I love girls.</p>
+					<p>Welcome in my private corner, I hope you feel comfortable here. ^^</p>
+					<br>
+					<p>I do programming on a daily basis. I'm interested in computers, creating graphics/drawings, (creating) music, art, video games and anime.</p>
+					<br>
+					<p>Sometimes I also take photos, I have a Kodak EasyShare DX4530 camera, manufactured in 2003, it's 2024 and it still works!</p>
+					<br>
+					<p>Other than that, I'm interested in socializing with people, editing Wikipedia articles, and (formerly) going to raves.</p>
+					<br>
+					<p>Feel free to ask anything you want! If you want contact me, maybe you can check <a href="https://t.me/sech1p">Telegram</a>, or my card where there are more ways to contact me. You can also leave me an email at <code>sech1p at disroot dot org</code></p>
+    		    </article>
+	    	</div>
+		    <?php include "templates/footer.php"; ?>
+    	</div>
+    </div>
+</body>
+</html>
