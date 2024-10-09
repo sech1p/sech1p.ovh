b3dee944949bf4635cfc00dfe860d90386c85227
sech1p <sech1p@disroot.org>
2024-10-09 17:03:59 +0200

    🔃 Expand Friends section

--- templates/header.php
+++ templates/header.php
@@ -18,6 +18,8 @@
 					<li><a href="https://t.me/sech1p">Telegram</a></li>
 					<li><b>Friends</b></li>
 				    <li><a href="https://ponymare.ovh"><img src="https://ponymare.ovh/res/trixie.gif" title="my cute gf emilia :3"></a></li>
+					<li><a href="https://anemonik.neocities.org"><img src="resources/anemonek.png" title="Anemonik"></a></li>
+					<li><a href="https://karibooru.love"><img src="resources/kariga.png" title="Karibooru"></a></li>
 					<li><b>My Button</b></li>
 					<li><img src="resources/sech1p.gif" /></li>
         			<li><b>Buttons (88x31)</b></li>
