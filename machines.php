d134d56016c0b7930f3155379bba2fefbec1c2d4
sech1p <sech1p@disroot.org>
2024-10-17 10:26:52 +0200

    ➕ Add machines page

---
+++ machines.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php include "templates/head.php"; ?>
+</head>
+<body>
+    <div id="top">
+	    <div id="main">
+		    <?php include "templates/header.php"; ?>
+		    <div id="subject">
+		        <article class="content">
+                    <h1>Machines</h1>
+                    <br>
+                    <h3>moon</h3>
+                    <p>My main desktop machine. You can read it parameters <a href="https://github.com/sech1p/dotfiles">on my dotfiles repository</a>. I using this to many things with I doing using computer (programming, music creation, graphics/drawing, video editing, etc.)</p>
+                    <br>
+                    <h3>HP Victus 16</h3>
+                    <p>My gaming laptop. Bought in 2022/3. I using this to play heavy games which from some reason can't run on <b>moon</b>.</p>
+                    <ul>
+                        <li><b>Processor:</b> AMD Ryzen 5 5600H</li>
+                        <li><b>Graphics Card:</b> AMD Radeon RX Vega 7</li>
+                        <li><b>Graphics Card:</b> NVidia GeForce RTX 3050 Ti Laptop</li>
+                        <li><b>Display:</b> 1920x1080, 144 Hz (16.1 inches)</li>
+                        <li><b>RAM:</b> 16 GB DDR4 (2x8 GB)</li>
+                        <li><b>Hard drive:</b> 256 GB NVMe (for system)</li>
+                        <li><b>Hard drive:</b> 512 GB NVMe (for data)</li>
+                        <li><b>Operating system:</b> Windows 11 Home</li>
+                    </ul>
+                    <br>
+                    <h3>ThinkPad L470</h3>
+                    <p>My work laptop. Bought in 2024. I using this to programming, graphics creation, attending lessons at the university, etc.</p>
+                    <ul>
+                        <li><b>Processor:</b> Intel Core i3-6100U</li>
+                        <li><b>Graphics Card:</b> Intel HD Graphics 520</li>
+                        <li><b>Display:</b> 1366x768, 60 Hz (14 inches)</li>
+                        <li><b>RAM:</b> 4 GB DDR4 (1x4 GB)</li>
+                        <li><b>Hard drive:</b> 128 GB SSD</li>
+                        <li><b>Operating system:</b> Windows 10 Pro</li>
+                    </ul>
+                    <br>
+                    <h3>Lenovo Tab M10 FHD Plus</h3>
+                    <p>My work/gaming tablet. Bought in 2022. I using this to creating notes for university and to play some mobile rhythm games (like Bandori, D4DJ, etc.).</p>
+                    <ul>
+                        <li><b>Display:</b> 1200x1920, 60 Hz (10.3 inches)</li>
+                        <li><b>RAM:</b> 4 GB</li>
+                        <li><b>Memory:</b> 128 GB</li>
+                        <li><b>Operating system:</b> Android 10</li>
+                    </ul>
+                    <br>
+                    <h3>Huawei Mate 10 late</h3>
+                    <p>My mobile phone. I using this to daily things when I'm not using a computer.</p>
+                    <ul>
+                        <li><b>Display:</b> 1080x2160, 60 Hz (5.9 inches)</li>
+                        <li><b>RAM:</b> 4 GB</li>
+                        <li><b>Memory:</b> 64 GB</li>
+                        <li><b>Operating system:</b> Android 8</li>
+                    </ul>
+    		    </article>
+	    	</div>
+		    <?php include "templates/footer.php"; ?>
+    	</div>
+    </div>
+</body>
+</html>
