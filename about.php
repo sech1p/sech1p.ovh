bbb73df8d1e33ec1c7ae7b6ca981287a83925d47
sech1p <sech1p@disroot.org>
2025-04-23 21:58:06 +0200

    🔃 Update about page
    Correcting my currently interests, update year to 2025, change my camera to my phone (sadly, my previous camera Kodak is dead :c) and update my contact e-mail

--- about.php
+++ about.php
@@ -13,13 +13,13 @@
 					<p>Eliza, polyamorous trans woman born in 2005 in East Poland. I am a lesbian, I love girls.</p>
 					<p>Welcome in my private corner, I hope you feel comfortable here. ^^</p>
 					<br>
-					<p>I do programming on a daily basis. I'm interested in computers, creating graphics/drawings, (creating) music, art, video games and anime.</p>
+					<p>I do programming on a daily basis. I'm interested in computers, creating graphics/drawings, creating music, psychonautics.</p>
 					<br>
-					<p>Sometimes I also take photos, I have a Kodak EasyShare DX4530 camera, manufactured in 2003, it's 2024 and it still works!</p>
+					<p>Sometimes I also take photos, I have a iPhone 16 (48 megapixels) for tooking photos</p>
 					<br>
-					<p>Other than that, I'm interested in socializing with people, editing Wikipedia articles, and (formerly) going to raves.</p>
+					<p>Other than that, I'm interested in socializing with people, and going to raves.</p>
 					<br>
-					<p>Feel free to ask anything you want! If you want contact me, maybe you can check <a href="https://t.me/sech1p">Telegram</a>, or my card where there are more ways to contact me. You can also leave me an email at <code>sech1p at disroot dot org</code></p>
+					<p>Feel free to ask anything you want! If you want contact me, maybe you can check <a href="https://t.me/sech1p">Telegram</a>, or my card where there are more ways to contact me. You can also leave me an email at <code>sech1p at icloud dot com</code></p>
     		    </article>
 	    	</div>
 		    <?php include "templates/footer.php"; ?>
