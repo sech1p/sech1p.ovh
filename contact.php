21252e90b75265ad2c23327f96838c23de1cf045
sech1p <sech1p@disroot.org>
2025-04-23 22:10:50 +0200

    🔃 Update contact page

--- contact.php
+++ contact.php
@@ -14,36 +14,9 @@
                     <ul>
                         <li>Discord - sech1p#0000</li>
                         <li>Telegram - <a href="https://t.me/sech1p">sech1p</a></li>
-                        <li>Signal - elizaemilia.47</li>
+                        <li>Signal - elizaemilia.43</li>
                         <li>Matrix - <code>@sech1p@mozilla.org</code></li>
-                        <li>XMPP - <code>sech1p at disroot dot org</code></li>
-                        <li>E-mail - <code>sech1p at disroot dot org</code></li>
-                    </ul>
-                    <br>
-                    <h2>Social Medias</h2>
-                    <ul>
-                        <li><a href="https://www.reddit.com/user/sech1p">Reddit</a></li>
-                        <li><a href="https://sech1p.bsky.social">Bluesky</a></li>
-                        <li>Twitter? nope</li>
-                        <li><a href="https://c.im/@sech1p">Mastodon</a></li>
-                    </ul>
-                    <br>
-                    <h3>Art/Graphics</h3>
-                    <ul>
-                        <li><a href="https://sech1p.deviantart.com">DeviantArt</a></li>
-                        <li><a href="https://www.furaffinity.net/user/sech1p">Fur Affinity</a></li>
-                    </ul>
-                    <br>
-                    <h3>Gaming</h3>
-                    <ul>
-                        <li><a href="https://psnprofiles.com/sech1p_">PlayStation</a></li>
-                        <li><a href="https://www.xboxgamertag.com/search/sech1p">Xbox</a></li>
-                        <li><a href="https://osu.ppy.sh/users/13861098">osu!</a></li>
-                    </ul>
-                    <br>
-                    <h3>Anime</h3>
-                    <ul>
-                        <li><a href="https://myanimelist.net/profile/sech1p">MyAnimeList</a></li>
+                        <li>E-mail - <code>sech1p at icloud dot com</code></li>
                     </ul>
     		    </article>
 	    	</div>
