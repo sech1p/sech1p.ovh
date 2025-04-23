c9616f360762979f99a4c6b89ad3a8f5f4928bfc
sech1p <sech1p@disroot.org>
2025-04-23 21:40:18 +0200

    🔃 Update index.php (welcome 2025 :D)

--- index.php
+++ index.php
@@ -10,14 +10,14 @@
 		    <div id="subject">
 		        <article class="content">
                     <h1>Hello, world!</h1>
-					<p>My name is Eliza, I'm 19 years old trans woman from Poland. I love girls.</p>
+					<p>My name is Eliza, I'm 20 years old trans woman from Poland. I love girls.</p>
 					<p>This is my personal website. My portfolio you can find at <a href="https://sech1p.art">sech1p.art</a></p>
 					<br>
 					<p>I'm do many things, if you want to know about some of them, maybe you want check <a href="about.php">About me</a> page?</p>
 					<br>
-					<p>You can also find my blog at <a href="blog.php">Blog</a> page. Enjoy and have fun ^^</p>
+					<p>You can also find my blogs on menu above. Enjoy and have fun ^^</p>
 					<br>
-					<p><i>Last update: 2024-10-17 10:28 (GMT+2)</i></p>
+					<p><i>Last update: 2025-04-23 21:38 (GMT+2)</i></p>
     		    </article>
 	    	</div>
 		    <?php include "templates/footer.php"; ?>
