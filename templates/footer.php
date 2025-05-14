0bfdac3cce38bc9be6843f0aae51d3e6732b65fe
sech1p <sech1p@disroot.org>
2025-05-15 00:43:35 +0200

    ➕ Add Made on Mac 88x31 button and Windows 98 Japanese banner to footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,1 +1,7 @@
-<footer id="footer">&copy; 2025 Eliza Emilia "sech1p" Semeniuk. Licensed under Apache-2.0 license.</footer>
+<footer id="footer">
+    &copy; 2025 Eliza Emilia "sech1p" Semeniuk. Licensed under Apache-2.0 license.
+    <br>
+	<img src="resources/made_on_mac.gif" />
+	<br>
+	<img src="resources/w98_banner.gif" />
+</footer>
