d27c0adedc01e244313240b9d32c8a5fb961aa23
sech1p <sech1p@disroot.org>
2025-07-10 22:54:19 +0200

    🌸 Improve head tags

--- templates/head.php
+++ templates/head.php
@@ -1,6 +1,49 @@
+<!-- UTF-8 Charset -->
 <meta charset="UTF-8">
+
+<!-- Viewport -->
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<!-- Style -->
 <link rel="stylesheet" type="text/css" href="style.css">
+
+<!-- Favicon -->
 <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
 <link rel="icon" type="image/x-icon" href="favicon.ico">
+<!-- Icons -->
+<link rel="icon" sizes="192x192" href="icon.png">
+<link rel="apple-touch-icon" href="icon.png">
+<link rel="mask-icon" href="icon.png" color="blue">
+
+<!-- Engine crawling -->
+<meta name="robots" content="index,follow">
+<meta name="googlebot" content="index,follow">
+
+<!-- Open Graph -->
+<!-- Open Graph: Facebook -->
+<meta property="og:url" content="https://sech1p.ovh">
+<meta property="og:type" content="website">
+<meta property="og:title" content="sech1p's corner">
+<meta property="og:image" content="https://sech1p.ovh/icon.png">
+<meta property="og:image:alt" content="Internet Explorer 6 logo">
+<meta property="og:description" content="Eliza 'sech1p' Semeniuk personal website">
+<meta property="og:site_name" content="sech1p">
+<meta property="og:locale" content="en_US">
+<!-- Open Graph: Twitter/X -->
+<meta name="twitter:card" content="summary">
+<meta name="twitter:site" content="@sech1p">
+<meta name="twitter:creator" content="@sech1p">
+<meta name="twitter:url" content="https://sech1p.ovh">
+<meta name="twitter:title" content="sech1p's corner">
+<meta name="twitter:description" content="Eliza 'sech1p' Semeniuk personal website">
+<meta name="twitter:image" content="https://secb1p.ovh/icon.png">
+<meta name="twitter:image:alt" content="Internet Explorer 6 logo.">
+
+<!-- Tech details -->
+<meta name="generator" content="Visual Studio Code + my hands">
+
+<!-- Meta -->
+<link rel="author" href="humans.txt">
+<link rel="license" href="LICENSE">
+<meta name="description" content="Eliza 'sech1p' Semeniuk personal website">
 <title>sech1p's corner</title>
